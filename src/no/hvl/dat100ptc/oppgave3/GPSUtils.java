a743db31ff9833076b621908bd882042c19b482b
Snadderbyte <69865773+Snadderbyte@users.noreply.github.com>
2020-10-13 13:45:35 +0200

    ferdig med oppg 4ish

--- src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
+++ src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
@@ -9,101 +9,113 @@ public class GPSUtils {
 
 	public static double findMax(double[] da) {
 
-		double max; 
-		
+		double max;
+
 		max = da[0];
-		
+
 		for (double d : da) {
 			if (d > max) {
 				max = d;
 			}
 		}
-		
+
 		return max;
 	}
 
 	public static double findMin(double[] da) {
 
-		double min;
+		double min = da[0];
 
-		// TODO - START
+		for (double d : da) {
+			if (d < min) {
+				min = d;
+			}
+		}
 
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO - SLUT
-
+		return min;
 	}
 
 	public static double[] getLatitudes(GPSPoint[] gpspoints) {
-
-		// TODO - START
+		double[] latitudes = new double[gpspoints.length];
 		
-		throw new UnsupportedOperationException(TODO.method());
+		for (int i = 0; i < latitudes.length; i++) {
+			latitudes[i] = gpspoints[i].getLatitude();
+		}
 		
-		// TODO - SLUTT
+		return latitudes;
 	}
 
 	public static double[] getLongitudes(GPSPoint[] gpspoints) {
 
-		// TODO - START
-
-		throw new UnsupportedOperationException(TODO.method());
+		double[] longitudes = new double[gpspoints.length];
 		
-		// TODO - SLUTT
+		for (int i = 0; i < longitudes.length; i++) {
+			longitudes[i] = gpspoints[i].getLongitude();
+		}
+		
+		return longitudes;
 
 	}
 
 	private static int R = 6371000; // jordens radius
 
 	public static double distance(GPSPoint gpspoint1, GPSPoint gpspoint2) {
-
-		double d;
-		double latitude1, longitude1, latitude2, longitude2;
-
-		// TODO - START
-
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO - SLUTT
-
+		
+		double latitude1 = Math.toRadians(gpspoint1.getLatitude()); 
+		double latitude2 = Math.toRadians(gpspoint2.getLatitude());
+		
+		double longitude1 = Math.toRadians(gpspoint1.getLongitude()); 
+		double longitude2 = Math.toRadians(gpspoint2.getLongitude());
+	
+		double a = Math.pow((Math.sin((latitude2-latitude1)/2)), 2) + Math.cos(latitude1) * Math.cos(latitude2)
+				* Math.pow((Math.sin((longitude2-longitude1)/2)), 2);
+		
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+		double d = 6371000 * c;
+		
+		return d;
 	}
 
 	public static double speed(GPSPoint gpspoint1, GPSPoint gpspoint2) {
 
-		int secs;
-		double speed;
-
-		// TODO - START
-
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO - SLUTT
-
+		int secs = gpspoint2.getTime() - gpspoint1.getTime();
+		
+		double dis = distance(gpspoint1, gpspoint2);
+		
+		double speed = dis / secs * 3.6; // distanse / tid
+		
+		return speed;
 	}
 
 	public static String formatTime(int secs) {
 
-		String timestr;
+		String timestr = "";
 		String TIMESEP = ":";
-
-		// TODO - START
-
-		throw new UnsupportedOperationException(TODO.method());
 		
-		// TODO - SLUTT
-
+		int hr = 0;
+		int min = 0;
+		
+		while (secs >= 60) {
+			min++;
+			secs -= 60;
+		} while (min >= 60) {
+			hr++;
+			min -= 60;
+		}
+		
+		timestr = String.format("  %02d:%02d:%02d", hr, min, secs);
+		
+		return timestr;
 	}
+
 	private static int TEXTWIDTH = 10;
 
 	public static String formatDouble(double d) {
 
 		String str;
-
-		// TODO - START
-
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO - SLUTT
 		
+		str = String.format("% 10.02f", d);
+		
+		return str;
 	}
 }
--- src/no/hvl/dat100ptc/oppgave4/GPSComputer.java
+++ src/no/hvl/dat100ptc/oppgave4/GPSComputer.java
@@ -8,9 +8,9 @@ import no.hvl.dat100ptc.oppgave2.GPSDataFileReader;
 import no.hvl.dat100ptc.oppgave3.GPSUtils;
 
 public class GPSComputer {
-	
+
 	private GPSPoint[] gpspoints;
-	
+
 	public GPSComputer(String filename) {
 
 		GPSData gpsdata = GPSDataFileReader.readGPSFile(filename);
@@ -21,21 +21,21 @@ public class GPSComputer {
 	public GPSComputer(GPSPoint[] gpspoints) {
 		this.gpspoints = gpspoints;
 	}
-	
+
 	public GPSPoint[] getGPSPoints() {
 		return this.gpspoints;
 	}
-	
+
 	// beregn total distances (i meter)
 	public double totalDistance() {
 
 		double distance = 0;
 
-		// TODO - START
+		for (int i = 1; i < gpspoints.length; i++) {
+			distance += GPSUtils.distance(gpspoints[i - 1], gpspoints[i]);
+		}
 
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO - SLUTT
+		return distance;
 
 	}
 
@@ -43,65 +43,74 @@ public class GPSComputer {
 	public double totalElevation() {
 
 		double elevation = 0;
+		double ele1 = 0;
+		double ele2 = 0;
 
-		// TODO - START
+		for (int i = 1; i < gpspoints.length; i++) {
 
-		throw new UnsupportedOperationException(TODO.method());
+			ele1 = gpspoints[i - 1].getElevation();
+			ele2 = gpspoints[i].getElevation();
 
-		// TODO - SLUTT
+			if ((ele2 - ele1) > 0) {
+				elevation += ele2 - ele1;
+			}
+		}
+
+		return elevation;
 
 	}
 
 	// beregn total tiden for hele turen (i sekunder)
 	public int totalTime() {
 
-		throw new UnsupportedOperationException(TODO.method());
+		int time = 0;
 
+		for (int i = 1; i < gpspoints.length; i++) {
+			time += gpspoints[i].getTime() - gpspoints[i - 1].getTime();
+		}
+
+		return time;
 	}
-		
+
 	// beregn gjennomsnitshastighets mellom hver av gps punktene
 
 	public double[] speeds() {
-		
-		// TODO - START		// OPPGAVE - START
-		
-		throw new UnsupportedOperationException(TODO.method());
 
-		// TODO - SLUTT
+		double[] speeds = new double[gpspoints.length - 1];
 
+		for (int i = 0; i < gpspoints.length - 1; i++) {
+			speeds[i] = GPSUtils.speed(gpspoints[i], gpspoints[i + 1]);
+		}
+		return speeds;
 	}
-	
+
 	public double maxSpeed() {
-		
+
 		double maxspeed = 0;
-		
-		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - SLUTT
-		
+
+		maxspeed = GPSUtils.findMax(speeds());
+
+		return maxspeed;
 	}
 
 	public double averageSpeed() {
 
 		double average = 0;
-		
-		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.method());
-		
-		// TODO - SLUTT
-		
+
+		for (int i = 0; i < speeds().length; i++) {
+			average += speeds()[i];
+		}
+
+		return average / speeds().length;
 	}
 
 	/*
-	 * bicycling, <10 mph, leisure, to work or for pleasure 4.0 bicycling,
-	 * general 8.0 bicycling, 10-11.9 mph, leisure, slow, light effort 6.0
-	 * bicycling, 12-13.9 mph, leisure, moderate effort 8.0 bicycling, 14-15.9
-	 * mph, racing or leisure, fast, vigorous effort 10.0 bicycling, 16-19 mph,
-	 * racing/not drafting or >19 mph drafting, very fast, racing general 12.0
-	 * bicycling, >20 mph, racing, not drafting 16.0
+	 * bicycling, <10 mph, leisure, to work or for pleasure 4.0 bicycling, general
+	 * 8.0 bicycling, 10-11.9 mph, leisure, slow, light effort 6.0 bicycling,
+	 * 12-13.9 mph, leisure, moderate effort 8.0 bicycling, 14-15.9 mph, racing or
+	 * leisure, fast, vigorous effort 10.0 bicycling, 16-19 mph, racing/not drafting
+	 * or >19 mph drafting, very fast, racing general 12.0 bicycling, >20 mph,
+	 * racing, not drafting 16.0
 	 */
 
 	// conversion factor m/s to miles per hour
@@ -113,31 +122,42 @@ public class GPSComputer {
 		double kcal;
 
 		// MET: Metabolic equivalent of task angir (kcal x kg-1 x h-1)
-		double met = 0;		
+		double met = 0;
 		double speedmph = speed * MS;
 
-		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.method());
+		if (speedmph < 10) {
+			met = 4;
+		} else if (speedmph > 10) {
+			met = 6;
+		} else if (speedmph > 12) {
+			met = 8;
+		} else if (speedmph > 14) {
+			met = 10;
+		} else if (speedmph > 16) {
+			met = 12;
+		} else if (speedmph > 20) {
+			met = 16;
+		}
 
-		// TODO - SLUTT
-		
+		kcal = met * weight * secs / 3600;
+
+		return kcal;
 	}
 
 	public double totalKcal(double weight) {
 
 		double totalkcal = 0;
 
-		// TODO - START
+		for (int i = 0; i < gpspoints.length - 1; i++) {
+			totalkcal += kcal(weight, gpspoints[i + 1].getTime() - gpspoints[i].getTime(),
+					GPSUtils.speed(gpspoints[i], gpspoints[i + 1]));
+		}
 		
-		throw new UnsupportedOperationException(TODO.method());
-
-		// TODO - SLUTT
-		
+		return totalkcal;
 	}
-	
+
 	private static double WEIGHT = 80.0;
-	
+
 	public void displayStatistics() {
 
 		System.out.println("==============================================");
@@ -145,9 +165,9 @@ public class GPSComputer {
 		// TODO - START
 
 		throw new UnsupportedOperationException(TODO.method());
-		
+
 		// TODO - SLUTT
-		
+
 	}
 
 }
